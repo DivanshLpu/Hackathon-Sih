65a6fae8739968ce292cf3dfdca64d21d9e3dc02
Priyanshuproj <davidmahlotra22@gmail.com>
2026-09-10 09:57:56 +0530

    edit with priyanshu

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({"message": "Hello from Harshal-sih!"})
+    return jsonify({"message": "Hello from PriyaNSHU THE G0UY-sih!"})
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0' , port=5000)
