1f19d74fd537c4eda8c59258df90f6c538fa1855
Divansh Gaba <chchchgggg2@gmail.com>
2026-09-10 09:50:19 +0530

    added flaks

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from Harshal-sih!")
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Hello from Harshal-sih!"})
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, host='0.0.0.0' , port=5000)
