c4d6081c0c8917a2f87f28f28c8bf32b24b3224f
Divansh Gaba <chchchgggg2@gmail.com>
2026-09-10 10:36:48 +0530

    added index.html

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Hello from Hackathon-sih!"})
+
+
+@app.route('/home', methods=['GET'])
+def home1():
+    # 2. Define the data you want to send to the HTML page
+    context_title = "Home Page"
+    username = "Alex"
+    
+    # 3. Render the file and pass variables (HTML_var=Python_var)
+    return render_template('index.html', title=context_title, user_name=username)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0' , port=5000)
